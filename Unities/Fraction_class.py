cff021e52571ed448ec14ad111392681599ae4b0
realxc <real@xc.com>
2024-02-10 17:58:54 +0800

    Fraction_class.py 完成原本应该是整数的分数的分类

--- Unities/Fraction_class.py
+++ Unities/Fraction_class.py
@@ -1,16 +1,25 @@
+from abc import ABC, abstractmethod
 
 
 class FractionABC(object):
+    """
+    抽象分数类, 用于无法判断类型时调用
+    """
     def __init__(self, numerator: int, denominator: int):
         """
         抽象分数类初始化
         :param numerator: 分子
         :param denominator: 分母
+        self.int_value: 整数值, 非整数为None
+        self.value: 值, 有必要时参加计算
         """
         self.numerator = numerator
         self.denominator = denominator
+        self.int_value = None
         self._verify_legitimacy()
         self._format()
+
+        self.value = self.numerator / self.denominator
 
     def _verify_legitimacy(self):
         """
@@ -44,6 +53,10 @@ class FractionABC(object):
         if self.symbol % 2 != 0:
             self.numerator = -self.numerator
 
+        # 如果可以是一个整数, 将 self.int_value 赋值
+        if self.denominator == 1:
+            self.int_value = self.numerator
+
     def _divide_common_factor(self):
         """
         同时除以公因数
@@ -66,7 +79,12 @@ class FractionABC(object):
         return f"({self.numerator}/{self.denominator})"
 
 
+class RegularFraction(FractionABC):
+    """
+    正则分数类, 标准的分数, 正常情况
+    """
+
 
 if __name__ == '__main__':
-    f = FractionABC(4, -8)
-    print(f)
+    f = FractionABC(4, -2)
+    print(f.int_value)
